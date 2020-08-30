1c0e01784b9e4d646bc878c60940dee80fc36940
Ngoc Thinh <hnthinh2909@gmail.com>
2020-08-30 21:16:07 +0700

    First commit, change homepage layout use @yield

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+    <!doctype html>
+<html lang="en">
+<head>
+    <title>@yield('title')</title>
+</head>
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
