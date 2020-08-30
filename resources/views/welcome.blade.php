1792f87defdef616cd1e892b831dcf2d810cacd5
Ngoc Thinh <hnthinh2909@gmail.com>
2020-08-30 23:12:27 +0700

    Create layouts signin, signup and connect database with mysql

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,5 +5,39 @@
 @endsection
 
 @section('content')
-    This is content!
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Sign Up</h3>
+            <form action={{route('signup')}} method="POST">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button class="btn btn-primary" type="submit">Submit</button>
+                <input type="hidden" name="_token" value="{{ Session::token() }}">
+            </form>
+        </div>
+        <div class="col-md-6">
+            <h3>Sign In</h3>
+            <form action="#" method="POST">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="text" name="password" id="password">
+                </div>
+                <button class="btn btn-primary" type="submit">Submit</button>
+            </form>
+        </div>
+    </div>
 @endsection
